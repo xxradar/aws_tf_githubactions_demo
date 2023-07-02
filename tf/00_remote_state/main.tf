575eb8b398d6e2ec9d08b3fd2b373966684315d3
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2023-07-02 11:55:21 +0200

    Update main.tf

--- tf/00_remote_state/main.tf
+++ tf/00_remote_state/main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "remote_state" {
-  bucket = "${var.prefix}-remote-state-${var.environment}"
+  bucket = "${var.prefix}-remote-state-${var.environment}-${var.studentid}"
   force_destroy = true
   tags = { 
-    Name = "${var.prefix}-remote-state-${var.environment}" 
+    Name = "${var.prefix}-remote-state-${var.environment}-${var.studentid}" 
     Environment = "${var.environment}"
   } 
 }
