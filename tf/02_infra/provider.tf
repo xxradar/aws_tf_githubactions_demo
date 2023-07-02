e15d68684bfcdf418033a9a55601d5e0fe7193ae
Philippe Bogaerts <philippe.bogaerts@radarhack.com>
2023-07-02 11:56:04 +0200

    Update provider.tf

--- tf/02_infra/provider.tf
+++ tf/02_infra/provider.tf
@@ -13,7 +13,7 @@ terraform {
 
   backend "s3" {
     region = "eu-west-3"
-    bucket = "xpertsummit-remote-state-development"
+    bucket = "xpertsummit-remote-state-development-studentXX"
     key    = "terraform.tfstate"
   }
 }
